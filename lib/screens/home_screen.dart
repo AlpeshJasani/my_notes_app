536f76930f36801cce9d9992389e572821baecd5
Alpesh Jasani <alpeshjasani777@gmail.com>
2024-11-29 22:05:09 +0530

    search,undo,sort added - ui fixed, all working fine

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart'; // Import Cupertino icons
 import 'package:share/share.dart'; // Share functionality package
 import 'package:intl/intl.dart'; // For date formatting
 import 'add_note_screen.dart';
@@ -16,8 +17,16 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   final List<Note> _notes = [];
-  Note? _lastDeletedNote; // To store the last deleted note
-  int? _lastDeletedNoteIndex; // To store the index of the last deleted note
+  List<Note> _filteredNotes = [];
+  String _searchQuery = ''; // Current search query
+  bool _isSearchActive = false; // Flag to check if search bar is active
+  bool _isSortedByDate = true; // Flag for sorting by date (newest to oldest)
+
+  @override
+  void initState() {
+    super.initState();
+    _filteredNotes = _notes; // Initialize filtered notes with all notes
+  }
 
   void _addNote() async {
     final newNote = await Navigator.push(
@@ -29,6 +38,7 @@ class _HomeScreenState extends State<HomeScreen> {
     if (newNote != null) {
       setState(() {
         _notes.add(newNote as Note);
+        _filteredNotes = _notes; // Update filtered notes
       });
     }
   }
@@ -37,24 +47,28 @@ class _HomeScreenState extends State<HomeScreen> {
     final editedNote = await Navigator.push(
       context,
       MaterialPageRoute(
-        builder: (context) => AddNoteScreen(note: _notes[index]),
+        builder: (context) => AddNoteScreen(note: _filteredNotes[index]),
       ),
     );
     if (editedNote != null) {
       setState(() {
-        _notes[index] = editedNote as Note;
+        final editedNoteObj = editedNote as Note;
+        _notes[_notes.indexOf(_filteredNotes[index])] = editedNoteObj;
+        _filteredNotes = _notes;
+        _isSearchActive = false;
+        _searchQuery = '';
       });
     }
   }
 
   void _deleteNoteWithUndo(int index) {
+    final Note deletedNote = _notes[index]; // Store the note that will be deleted
+
     setState(() {
-      _lastDeletedNote = _notes[index];
-      _lastDeletedNoteIndex = index;
       _notes.removeAt(index);
+      _filteredNotes = _notes; // Update filtered notes after deletion
     });
 
-    // Show Snackbar with undo option
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
         content: const Text('Note deleted'),
@@ -62,15 +76,13 @@ class _HomeScreenState extends State<HomeScreen> {
           label: 'UNDO',
           onPressed: () {
             setState(() {
-              if (_lastDeletedNote != null && _lastDeletedNoteIndex != null) {
-                _notes.insert(_lastDeletedNoteIndex!, _lastDeletedNote!);
-                _lastDeletedNote = null; // Clear last deleted note
-                _lastDeletedNoteIndex = null; // Clear index
-              }
+              // If "UNDO" is pressed, restore the deleted note
+              _notes.insert(index, deletedNote); // Insert the deleted note back at the original index
+              _filteredNotes = _notes; // Update filtered notes
             });
           },
         ),
-        duration: const Duration(seconds: 3), // Show Snackbar for 3 seconds
+        duration: const Duration(seconds: 3),
       ),
     );
   }
@@ -79,13 +91,60 @@ class _HomeScreenState extends State<HomeScreen> {
     Share.share('Title: ${note.title}\n\nContent: ${note.content}');
   }
 
+  void _searchNotes(String query) {
+    setState(() {
+      _searchQuery = query;
+      _filteredNotes = _notes.where((note) {
+        return note.title.toLowerCase().contains(query.toLowerCase()) ||
+            note.content.toLowerCase().contains(query.toLowerCase());
+      }).toList();
+    });
+  }
+
+  void _sortNotes() {
+    setState(() {
+      if (_isSortedByDate) {
+        _notes.sort((a, b) => b.dateTime.compareTo(a.dateTime)); // Sort by newest to oldest
+      } else {
+        _notes.sort((a, b) => a.dateTime.compareTo(b.dateTime)); // Sort by oldest to newest
+      }
+      _filteredNotes = _notes; // Update filtered notes after sorting
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('My Notes'),
         actions: [
-          // Toggle theme button with dynamic color based on the current theme
+          IconButton(
+            icon: Icon(_isSearchActive ? Icons.close : Icons.search),
+            color: widget.isDarkMode ? Colors.white : Colors.black,
+            onPressed: () {
+              setState(() {
+                if (_isSearchActive) {
+                  _isSearchActive = false;
+                  _searchQuery = '';
+                  _filteredNotes = _notes;
+                } else {
+                  _isSearchActive = true;
+                }
+              });
+            },
+          ),
+          IconButton(
+            icon: Icon(
+              _isSortedByDate ? CupertinoIcons.sort_down : CupertinoIcons.sort_up, // Cupertino icons for sorting
+              color: widget.isDarkMode ? Colors.white : Colors.black,
+            ),
+            onPressed: () {
+              setState(() {
+                _isSortedByDate = !_isSortedByDate;
+                _sortNotes();
+              });
+            },
+          ),
           IconButton(
             icon: Icon(
               widget.isDarkMode ? Icons.light_mode : Icons.dark_mode,
@@ -95,60 +154,79 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ],
       ),
-      body: ListView.builder(
-        itemCount: _notes.length,
-        itemBuilder: (context, index) {
-          final note = _notes[index];
-          final formattedDate = DateFormat('MMM d, yyyy - hh:mm a').format(note.dateTime);
-
-          return Card(
-            margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-            elevation: 4,
-            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-            child: ListTile(
-              contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-              title: Text(
-                note.title,
-                style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+      body: Column(
+        children: [
+          if (_isSearchActive)
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              child: TextField(
+                autofocus: true,
+                decoration: const InputDecoration(
+                  labelText: 'Search notes...',
+                  border: OutlineInputBorder(),
+                  suffixIcon: Icon(Icons.search),
+                ),
+                onChanged: _searchNotes,
               ),
-              subtitle: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(top: 8),
-                    child: Text(
-                      note.content,
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                      style: const TextStyle(color: Colors.grey),
-                    ),
-                  ),
-                  const SizedBox(height: 8),
-                  Text(
-                    'Saved: $formattedDate',
-                    style: const TextStyle(fontSize: 12, color: Colors.grey),
-                  ),
-                ],
-              ),
-              trailing: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  IconButton(
-                    icon: const Icon(Icons.share),
-                    color: Colors.green,
-                    onPressed: () => _shareNote(note),
-                  ),
-                  IconButton(
-                    icon: const Icon(Icons.delete),
-                    color: Colors.red,
-                    onPressed: () => _deleteNoteWithUndo(index),
-                  ),
-                ],
-              ),
-              onTap: () => _editNote(index),
             ),
-          );
-        },
+          Expanded(
+            child: ListView.builder(
+              itemCount: _filteredNotes.length,
+              itemBuilder: (context, index) {
+                final note = _filteredNotes[index];
+                final formattedDate = DateFormat('MMM d, yyyy - hh:mm a').format(note.dateTime);
+
+                return Card(
+                  margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                  elevation: 4,
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+                  child: ListTile(
+                    contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                    title: Text(
+                      note.title,
+                      style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                    ),
+                    subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(top: 8),
+                          child: Text(
+                            note.content,
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                            style: const TextStyle(color: Colors.grey),
+                          ),
+                        ),
+                        const SizedBox(height: 8),
+                        Text(
+                          'Saved: $formattedDate',
+                          style: const TextStyle(fontSize: 12, color: Colors.grey),
+                        ),
+                      ],
+                    ),
+                    trailing: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        IconButton(
+                          icon: const Icon(Icons.share),
+                          color: Colors.green,
+                          onPressed: () => _shareNote(note),
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.delete),
+                          color: Colors.red,
+                          onPressed: () => _deleteNoteWithUndo(index),
+                        ),
+                      ],
+                    ),
+                    onTap: () => _editNote(index),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _addNote,
